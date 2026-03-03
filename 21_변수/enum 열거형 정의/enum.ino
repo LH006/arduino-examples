0781a2092ded55bd1fb49dd8f2963297b75bdd8e
LH006 <lh0000006@gmail.com>
2026-03-03 22:14:20 +0900

    main

---
+++ 21_변수/enum 열거형 정의/enum.ino
@@ -0,0 +1,23 @@
+// 열거형 정의
+enum
+{
+  HEAD1,
+  HEAD2,
+  HEAD3,
+  DATASIZE,
+  CMD,
+  ROLL,
+  PITCH,
+  YAW,
+  THROTTLE,
+  AUX,
+  CRC,
+  PACKETSIZE,
+};
+// HEAD1 = 0번째
+// PACKETSIZE 11번째
+uint8_t mspPacket[PACKETSIZE]
+
+
+
+//========================================

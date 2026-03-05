f87669d16ef1c2132d74180e963f15a2407fc4ab
LH006 <lh0000006@gmail.com>
2026-03-05 15:39:14 +0900

    main

--- 23_조건문/if()_else_ifdef/if()_else().ino
+++ 23_조건문/if()_else_ifdef/if()_else().ino
@@ -16,9 +16,29 @@ void loop() {
     delay(500);
   }
 #endif /////////////////////////////////////////////////////////
-  Serial.println("Test");
-  delay(500);
 }
+
+
+
+  if (warn_failsafe == 2)      beep_code('L','N','N','D');                 //failsafe "find me" signal
+  else if (warn_failsafe == 1) beep_code('S','M','L','M');                 //failsafe landing active         
+  else if (warn_noGPSfix == 1) beep_code('S','S','N','M');       
+  else if (beeperOnBox == 1)   beep_code('S','S','S','M');                 //beeperon
+  else if (warn_vbat == 4)     beep_code('S','M','M','D');       
+  else if (warn_vbat == 2)     beep_code('S','S','M','D');       
+  else if (warn_vbat == 1)     beep_code('S','M','N','D');           
+  else if (warn_runtime == 1 && f.ARMED == 1)beep_code('S','S','M','N'); //Runtime warning            
+  else if (toggleBeep > 0)     beep(50);                                   //fast confirmation beep
+  else { 
+    buzzerIsOn = 0;
+    BUZZERPIN_OFF;
+  } 
+
+
+
+
+
+
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void wheel_left_right(byte rx_byte2)

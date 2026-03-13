d57bba775f44a710f0e8d6652765b8db08637bbb
LH006 <lh0000006@gmail.com>
2026-03-13 23:31:25 +0900

    Update struct.ino

--- 21_변수/struct/struct.ino
+++ 21_변수/struct/struct.ino
@@ -3,23 +3,24 @@ struct joystick_str
 };
 struct joystick_str joustick_1[];
 struct joystick_str joustick_2[];
-
-// Max size of this struct is 32 bytes - NRF24L01 buffer limit
-struct data_package_str
+////////////////////////////////////////////////////////////////
+struct data_status
 {
-  byte j1PotX;
-  byte j1PotY;
-  byte j1Button;
-  byte j2PotX;
-  byte j2PotY;
-  byte j2Button;
-  byte pot1;
-  byte pot2;
-  byte tSwitch1;
-  byte tSwitch2;
-  byte button1;
-  byte button2;
-  byte button3;
-  byte button4;
+  int16_t int_throttle;
+  int16_t int_pitch;
+  int16_t roll;
+  int16_t yaw;
+  boolean joy_button_l;
+  boolean joy_button_r; // = false
+  boolean aux1_button;
+  boolean aux2_button;
+  float battery_voltage;
 };
-struct data_package_str data; // Create a variable with the above structure
+struct data_status str_data_status;	// 생성
+
+
+//사용방법
+  str_data_status.joy_button_l = digitalRead(PIN_JOYSTICK_SW_L);
+  str_data_status.joy_button_r = digitalRead(PIN_JOYSTICK_SW_R);
+  str_data_status.battery_voltage = battery_value(0);
+/////////////////////////////////////////////////////////////////

d79e67d2dc5d65004c2d223c83ba785d5c13b4b2
가가멜 <121926881+LH006@users.noreply.github.com>
2026-03-21 13:42:49 +0900

    for.ino 업데이트

--- 23_조건문/for/for.ino
+++ 23_조건문/for/for.ino
@@ -1,4 +1,12 @@
+////////////////////////////////////
   for (int joystick = 0; joystick < 255; joystick++) {
     Serial.println(joystick);
     radio.write( &joystick, sizeof(joystick) );
      }
+////////////////////////////////////
+void InitIO(){ 
+  for(int i = 0; i < 17; i++) pinMode(i, INPUT); 
+  pinMode(virbrationMotorPin,OUTPUT);
+  digitalWrite(virbrationMotorPin,LOW);  // Stop shacking of the gamepad
+}
+////////////////////////////////////

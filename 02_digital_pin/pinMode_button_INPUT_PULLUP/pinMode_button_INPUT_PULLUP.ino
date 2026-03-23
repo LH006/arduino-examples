82d57af686420a42b3358bd90d725f2ce23fce02
ss <lh0000006@gmail.com>
2026-03-23 14:40:28 +0900

    정리

--- 02_digital_pin/pinMode_button_INPUT_PULLUP/pinMode_button_INPUT_PULLUP.ino
+++ 02_digital_pin/pinMode_button_INPUT_PULLUP/pinMode_button_INPUT_PULLUP.ino
@@ -1,27 +1,3 @@
-/*
-  Button
-
-  Turns on and off a light emitting diode(LED) connected to digital pin 13,
-  when pressing a pushbutton attached to pin 2.
-
-  The circuit:
-  - LED attached from pin 13 to ground through 220 ohm resistor
-  - pushbutton attached to pin 2 from +5V
-  - 10K resistor attached to pin 2 from ground
-
-  - Note: on most Arduinos there is already an LED on the board
-    attached to pin 13.
-
-  created 2005
-  by DojoDave <http://www.0j0.org>
-  modified 30 Aug 2011
-  by Tom Igoe
-
-  This example code is in the public domain.
-
-  https://www.arduino.cc/en/Tutorial/BuiltInExamples/Button
-*/
-
 // constants won't change. They're used here to set pin numbers:
 const int buttonPin = 2;     // the number of the pushbutton pin
 const int ledPin =  13;      // the number of the LED pin
@@ -30,6 +6,8 @@ const int ledPin =  13;      // the number of the LED pin
 int buttonState = 0;         // variable for reading the pushbutton status
 
 void setup() {
+  //pinMode(2, INPUT_PULLUP); //풀업
+
   // initialize the LED pin as an output:
   pinMode(ledPin, OUTPUT);
   // initialize the pushbutton pin as an input:
@@ -49,3 +27,10 @@ void loop() {
     digitalWrite(ledPin, LOW);
   }
 }
+
+/*
+  // initialize the LED pins:
+  for (int thisPin = 2; thisPin < 7; thisPin++) {
+    pinMode(thisPin, OUTPUT);
+  }
+*/
